b39c52ccae983d5842dcac8ed585e675523536ef
Zengyujun <1041827696@qq.com>
2019-09-11 11:23:49 +0800

    0911

---
+++ tyty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tyty
+//
+//  Created by 曾俞钧 on 2019/9/11.
+//  Copyright © 2019 曾俞钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tyty/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  tyty
+//
+//  Created by 曾俞钧 on 2019/9/11.
+//  Copyright © 2019 曾俞钧. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
